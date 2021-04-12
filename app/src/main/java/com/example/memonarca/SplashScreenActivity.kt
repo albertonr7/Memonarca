1ac9d3907d6d8a8f74ed4df3c1f4e7187f62eb11
Alberto Nieto Rocha <51422875+albertonr7@users.noreply.github.com>
2021-04-12 17:12:54 -0600

    Pantalla de carga

---
+++ app/src/main/java/com/example/memonarca/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.memonarca
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+// Displays loading screen and open menu
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //this.handler = Handler()
+        this.handler.postDelayed({
+            val intent = Intent( this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000) // Delay (4 seconds)
+    }
+}
